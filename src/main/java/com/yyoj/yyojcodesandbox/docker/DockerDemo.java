2ef9591cad36b098769718c581148f4b584871d8
杨建成 <11967785+yang-jiancheng@user.noreply.gitee.com>
2025-10-30 21:45:57 +0800

    基于docker的代码沙箱

--- src/main/java/com/yyoj/yyojcodesandbox/docker/DockerDemo.java
+++ src/main/java/com/yyoj/yyojcodesandbox/docker/DockerDemo.java
@@ -2,13 +2,27 @@ package com.yyoj.yyojcodesandbox.docker;
 
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.command.PingCmd;
+import com.github.dockerjava.api.command.PullImageCmd;
+import com.github.dockerjava.api.command.PullImageResultCallback;
+import com.github.dockerjava.api.model.PullResponseItem;
 import com.github.dockerjava.core.DockerClientBuilder;
 
 public class DockerDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         //获取默认的Docker Client
         DockerClient dockerClient = DockerClientBuilder.getInstance().build();
-        PingCmd pingCmd = dockerClient.pingCmd();
-        System.out.println(pingCmd.exec());
+//        PingCmd pingCmd = dockerClient.pingCmd();
+//        System.out.println(pingCmd.exec());
+        String image = "hello-world";
+        PullImageCmd pullImageCmd = dockerClient.pullImageCmd(image);
+        PullImageResultCallback pullImageResultCallback = new PullImageResultCallback(){
+            @Override
+            public void onNext(PullResponseItem item) {
+                System.out.println("下载镜像" + item.getStatus());
+                super.onNext(item);
+            }
+        };
+        pullImageCmd.exec(pullImageResultCallback).awaitCompletion();
+        System.out.println("下载完成！");
     }
 }
