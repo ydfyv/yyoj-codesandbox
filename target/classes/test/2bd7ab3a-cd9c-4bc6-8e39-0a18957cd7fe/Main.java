983479bdf3a0ba0f3d2ecbbd48d94943551de033
杨建成 <11967785+yang-jiancheng@user.noreply.gitee.com>
2026-01-26 14:06:52 +0800

    add Dockerfile

---
+++ target/classes/test/2bd7ab3a-cd9c-4bc6-8e39-0a18957cd7fe/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String arg1 = args[0];
+        String arg2 = args[1];
+        System.out.println("输出的结果是：" + (Integer.parseInt(arg1) + Integer.parseInt(arg2)));
+    }
+
+}
